4bff58e8556360048a724937ef861f156cf34b99
alfnmbrkh <alfinmubarkah@gmail.com>
2025-03-10 10:17:23 +0800

    Pertemuan 1 : Pengenalan & Operasi Input/Output

---
+++ Pertemuan 1/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+
+    cin >> nim;
+
+    cout << "Masukkan IP: ";
+    cin >> ip;
+    
+    cout << "Masukkan jenis kelamin (L/P): ";
+    jeniskelamin = getche(); //agar karakter langsung tampil jadi ga perlu tekan enter
+
+    //untuk output
+    cout << nama << endl;
+    cout << kom << endl;
+    cout << nim << endl;
+    cout << ip << endl;
+    putchar(jeniskelamin); //untuk menampilkan jenis kelamin
+}
