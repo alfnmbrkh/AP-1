50ba6a079ae482c9ae99f6b70a722c86ca2b24ab
alfnmbrkh <alfinmubarkah@gmail.com>
2025-05-23 23:29:42 +0700

    Update latihan2.cpp

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    system("cls");
-
+    system("cls"); //clear screen
+    //deklarasi variabel
     string kalimat;
     cout << "Masukkan kalimat: "; getline(cin, kalimat);
 
-    for (int i = 0; i < kalimat.length(); i++) {
-        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u'){
-            kalimat[i] = toupper(kalimat[i]);
+    for (int i = 0; i < kalimat.length(); i++) { // jika i<kalimat.length() masih terpenuhi akan dijalankan
+        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u'){ // jika terdapat char di kalimat yang berupa a,i,u,e,o
+            kalimat[i] = toupper(kalimat[i]); //kalimat akan dikapitalkan
         }
     }
     cout << "Kalimat hasil konversi: " << kalimat << endl;
